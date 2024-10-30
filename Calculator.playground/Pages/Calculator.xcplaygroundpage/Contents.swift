8c531b0f15ed8ec59afecc9ea56ce7c0ebf8b04c
chhue <chhue96@gmail.com>
2024-10-30 22:38:58 +0900

    [Feat] 연산 메소드 선언
    덧셈, 뺄셈, 곱셈, 나눗셈에 대해 각각 메소드 정의
    인스턴스 생성하여 메소드 테스트
    빈 파일 삭제 (파일명 : "Calculatoro.xcplaygroundpage")

--- Calculator.playground/Pages/Calculator.xcplaygroundpage/Contents.swift
+++ Calculator.playground/Pages/Calculator.xcplaygroundpage/Contents.swift
@@ -2,7 +2,33 @@ import UIKit
 
 class Calculator {
     
+    func add(_ firstInput: Int, to secondInput: Int) -> Int {
+        return firstInput + secondInput
+    }
+    
+    func subtract(_ firstInput: Int, from secondInput: Int) -> Int {
+        return firstInput - secondInput
+    }
+    
+    func mutilply(_ firstInput: Int, by secondInput: Int) -> Int {
+        return firstInput * secondInput
+    }
+    
+    func divide(_ firstInput: Int, by secondInput: Int) -> Int {
+        return firstInput / secondInput
+    }
+    
+    /** 오류 가능 예외상황
+     * 1. 파라미터로 받은 값이 Int 값이 아닐 경우
+     * 2. 나눗셈 - 두 값의 결과가 Int 값이 아닐 경우
+     * 3. 파라미터로 받은 값이 Int로 표현할 수 있는 자리수가 넘어갈 경우
+     **/
 }
 
 
 let calculator = Calculator()
+
+calculator.add(5, to: 6)
+calculator.subtract(9, from: 10)
+calculator.mutilply(11, by: 12)
+calculator.divide(13, by: 14)
