499ca6e1dae00e5ca38fd80e9b57c5e6f3e4e714
chhue <chhue96@gmail.com>
2024-10-30 22:44:01 +0900

    [Feat] 나머지 연산 메소드 선언

--- Calculator.playground/Pages/Calculator.xcplaygroundpage/Contents.swift
+++ Calculator.playground/Pages/Calculator.xcplaygroundpage/Contents.swift
@@ -18,6 +18,10 @@ class Calculator {
         return firstInput / secondInput
     }
     
+    func modulo(_ firstInput: Int, by secondInput: Int) -> Int {
+        return firstInput % secondInput
+    }
+    
     /** 오류 가능 예외상황
      * 1. 파라미터로 받은 값이 Int 값이 아닐 경우
      * 2. 나눗셈 - 두 값의 결과가 Int 값이 아닐 경우
@@ -32,3 +36,4 @@ calculator.add(5, to: 6)
 calculator.subtract(9, from: 10)
 calculator.mutilply(11, by: 12)
 calculator.divide(13, by: 14)
+calculator.modulo(13, by: 14)
